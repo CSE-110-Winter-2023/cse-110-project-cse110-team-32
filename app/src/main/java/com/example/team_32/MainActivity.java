611baa1e41109448c856ce5b683710d4a4184e2c
KatherineNiu <cniu@ucsd.edu>
2023-02-12 20:56:18 -0800

    added self-location

--- app/src/main/java/com/example/team_32/MainActivity.java
+++ app/src/main/java/com/example/team_32/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.team_32;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.app.Activity;
+import android.content.pm.PackageManager;
 import android.graphics.Matrix;
 import android.graphics.drawable.Animatable;
 import android.os.Bundle;
@@ -10,20 +13,44 @@ import android.view.animation.Transformation;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import android.Manifest;
+
+import org.w3c.dom.Text;
+
 public class MainActivity extends AppCompatActivity {
     private OrientationService orientationService;
+
+    private LocationService locationService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         orientationService = OrientationService.singleton(this);
         ImageView comFace = findViewById(R.id.compassFace);
 
         orientationService.getOrientation().observe(this, ori -> {
             float degrees = (float) Math.toDegrees((double) ori);
-            comFace.setRotation(degrees);
+            comFace.setRotation(-degrees);
         });
+
+
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 200);
+        }
+
+        locationService= LocationService.singleton(this);
+
+        TextView textView = (TextView) findViewById(R.id.serviceTextView);
+
+        locationService.getLocation().observe(this, loc -> {
+            textView.setText(Double.toString(loc.first) + " , " +
+                    Double.toString(loc.second));
+        });
+
     }
 
     @Override
