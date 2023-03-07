89c33322a996368623e8a2af3851e08cea9b18f0
carolynzhangxy <carolynzhangxy@gmail.com>
2023-03-07 12:56:23 -0800

    adding freinds and username

--- app/src/main/java/com/example/team_32/Utilities.java
+++ app/src/main/java/com/example/team_32/Utilities.java
@@ -1,5 +1,7 @@
 package com.example.team_32;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 public class Utilities {
@@ -10,5 +12,21 @@ public class Utilities {
         } catch (NumberFormatException e) {
             return Optional.empty();
         }
+    }
+    public static List<FakeUser> getFakeUsers(){
+        List<FakeUser> fakeUsers = new ArrayList<>();
+        for(int i = 0; i < 10; i++){
+            FakeUser fakeUser = new FakeUser("User " + i, "" + i);
+            fakeUsers.add(fakeUser);
+        }
+        return fakeUsers;
+    }
+    public static List<FakeUser> getFakeFriends(){
+        List<FakeUser> fakeFriends = new ArrayList<>();
+        for(int i = 0; i < 3; i++){
+            FakeUser fakeFriend = new FakeUser("Friend " + i, "" + i);
+            fakeFriends.add(fakeFriend);
+        }
+        return fakeFriends;
     }
 }
